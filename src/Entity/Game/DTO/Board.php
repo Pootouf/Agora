aeb345f34783cf1f7ad718fe616cac3950ebad7c
montaerw <erwann.montagne@univ-rouen.fr>
2023-12-14 16:34:33 +0100

    Created Main Board, Player, PersonalBoard classes

--- src/Entity/Game/DTO/Board.php
+++ src/Entity/Game/DTO/Board.php
@@ -4,6 +4,10 @@ abstract class Board {
 
     private int $BoardId;
 
+    public function __construct(int $BoardId){
+        $this->BoardId = $BoardId;
+    }
+
     public function getBoardId(): int {
         return $this->BoardId;
     }
--- src/Entity/Game/DTO/MainBoard.php
+++ src/Entity/Game/DTO/MainBoard.php
@@ -5,7 +5,8 @@ class MainBoard extends Board {
     private int $gameId;
     
 
-    public function __construct(int $gameId) {
+    public function __construct(int $gameId, int $boardId) {
+        parent::__construct($boardId);
         $this->gameId = $gameId;
     }
 
