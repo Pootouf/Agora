ee5ed09e8c66fafd1d3f2488aaf7a572cd0ddb02
bingieti <etienne.binginot@univ-rouen.fr>
2023-12-14 17:31:50 +0100

    Feature : update Game.php with new database system (no array)

--- src/Entity/Game/DTO/Game.php
+++ src/Entity/Game/DTO/Game.php
@@ -4,14 +4,8 @@ class Game {
 
     private int $gameId;
 
-    private Array $players;
-
-    private MainBoard $mainBoard;
-
-    public function __construct(int $gameId, Array $players, MainBoard $mainBoard) {
+    public function __construct(int $gameId) {
         $this->gameId = $gameId;
-        $this->players = $players;
-        $this->mainBoard = $mainBoard;
     }
 
     public function getGameId(): int
@@ -19,29 +13,9 @@ class Game {
         return $this->gameId;
     }
 
-    public function getPlayers(): array
-    {
-        return $this->players;
-    }
-
-    public function getMainBoard(): MainBoard
-    {
-        return $this->mainBoard;
-    }
-
     public function setGameId(int $gameId): void
     {
         $this->gameId = $gameId;
-    }
-
-    public function setPlayers(array $players): void
-    {
-        $this->players = $players;
-    }
-
-    public function setMainBoard(MainBoard $mainBoard): void
-    {
-        $this->mainBoard = $mainBoard;
     }
 
 }
