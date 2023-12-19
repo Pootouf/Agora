75a4976f02bc399f295c115749506f8ad3688640
bingieti <etienne.binginot@univ-rouen.fr>
2023-12-19 21:13:10 +0100

    Fix : remove unused import

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,11 @@ module.exports = {
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
-    "./node_modules/tw-elements/dist/js/**/*.js"
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('@tailwindcss/forms')({
-      strategy: 'class',
-    }),
-    require('tw-elements/dist/plugin.cjs')
+    require('@tailwindcss/forms'),
   ],
 }
