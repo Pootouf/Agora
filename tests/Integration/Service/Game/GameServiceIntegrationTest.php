e15bb2e8f9724af718cc042a6a0d398fa1e1f2ad
Etienne Binginot <etienne.binginot@univ-rouen.fr>
2024-01-20 21:45:36 +0100

    AV-43 test: add integration failures tests and modify success test

--- tests/Integration/Service/Game/GameServiceIntegrationTest.php
+++ tests/Integration/Service/Game/GameServiceIntegrationTest.php
@@ -4,6 +4,7 @@ namespace App\Tests\Integration\Service\Game;
 
 use App\Repository\Game\SixQP\PlayerSixQPRepository;
 use App\Service\Game\GameService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class GameServiceIntegrationTest extends KernelTestCase
@@ -11,9 +12,6 @@ class GameServiceIntegrationTest extends KernelTestCase
 
     public function testCreateSixQPGameValidWithValidPlayer(): void
     {
-        $kernel = self::bootKernel();
-        $this->assertSame('test', $kernel->getEnvironment());
-
         $gameService = static::getContainer()->get(GameService::class);
 
         $players = ['test', 'test', 'test', 'test']; //TODO : add real users
@@ -22,7 +20,7 @@ class GameServiceIntegrationTest extends KernelTestCase
         $playerRepository = static::getContainer()->get(PlayerSixQPRepository::class);
         $this->assertInstanceOf(PlayerSixQPRepository::class, $playerRepository);
 
-        $sixQPPlayers = $playerRepository->findAll();
+        $sixQPPlayers = $playerRepository->findBy(['game' => $game->getId()]);
         $this->assertSame(count($sixQPPlayers), count($players));
 
         $rows = $game->getRowSixQPs();
@@ -33,5 +31,24 @@ class GameServiceIntegrationTest extends KernelTestCase
             $this->assertSame(count($row->getCards()), 0);
             $this->assertSame($row->getPosition(), $i++);
         }
+    }
+
+    public function testCreateSixQPGameValidWithTooManyPlayer(): void
+    {
+        $gameService = static::getContainer()->get(GameService::class);
+
+        $players = ['test', 'test', 'test', 'test', 'test', 'test', 'test', 'test'
+            , 'test', 'test', 'test', 'test', 'test', 'test']; //TODO : add real users
+        $this->expectException(Exception::class);
+        $gameService->createSixQPGame($players);
+    }
+
+    public function testCreateSixQPGameValidWithNotEnoughPlayer(): void
+    {
+        $gameService = static::getContainer()->get(GameService::class);
+
+        $players = ['test']; //TODO : add real users
+        $this->expectException(Exception::class);
+        $gameService->createSixQPGame($players);
     }
 }
