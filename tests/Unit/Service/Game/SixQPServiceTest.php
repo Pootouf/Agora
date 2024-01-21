e99af1fb8f92a144b4d1b09729fb5822ecc0b398
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-01-21 10:56:54 +0100

    AV-34 : Fixed unit tests for chooseCard

--- tests/Unit/Service/Game/SixQPServiceTest.php
+++ tests/Unit/Service/Game/SixQPServiceTest.php
@@ -3,42 +3,37 @@
 namespace App\Tests\Service\Game;
 
 use App\Entity\Game\SixQP\CardSixQP;
-use App\Entity\Game\SixQP\ChosenCardSixQP;
 use App\Entity\Game\SixQP\GameSixQP;
 use App\Entity\Game\SixQP\PlayerSixQP;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use PHPUnit\Framework\TestCase;
 use App\Service\Game\SixQPService;
 
-class SixQPServiceTest extends KernelTestCase
+class SixQPServiceTest extends TestCase
 {
-
+    private SixQPService $sixQPService;
     // Unit Tests for chooseCard
     public function testChooseCardWhenCardNotOwned(): void
     {
-        $sixQPService = static::getContainer()->get(SixQPService::class);
         $player = new PlayerSixQP();
         $card = new CardSixQP();
         $this->expectException(Exception::class);
-        $sixQPService->chooseCard($player, $card);
+        $this->sixQPService->chooseCard($player, $card);
     }
 
     public function testChooseCardWhenPlayerAlreadyChose(): void
     {
-        $sixQPService = static::getContainer()->get(SixQPService::class);
         $player = new PlayerSixQP();
         $oldCard = new CardSixQP();
         $newCard = new CardSixQP();
         $this->expectException(Exception::class);
-        $sixQPService->chooseCard($player, $oldCard);
-        $sixQPService->chooseCard($player, $newCard);
+        $this->sixQPService->chooseCard($player, $oldCard);
+        $this->sixQPService->chooseCard($player, $newCard);
     }
 
     public function testChooseCardWhenValid(): void
     {
-        $sixQPService = static::getContainer()->get(SixQPService::class);
-        $entityManager = static::getContainer()->get(EntityManagerInterface::class);
 
         $card = new CardSixQP();
         $card->setValue(1);
@@ -52,20 +47,19 @@ class SixQPServiceTest extends KernelTestCase
         $player->setUsername("test");
         $cards = $player->getCards();
 
-        $entityManager->persist($card);
-        $entityManager->persist($game);
-        $entityManager->persist($player);
-        $entityManager->flush();
-
         $this->assertTrue($cards->contains($card));
         $this->assertNull($player->getChosenCardSixQP());
-        $sixQPService->chooseCard($player, $card);
+        $this->sixQPService->chooseCard($player, $card);
+        $this->assertNotNull($player->getChosenCardSixQP());
         $this->assertFalse($player->getChosenCardSixQP()->isState());
         $cards = $player->getCards();
         $this->assertFalse($cards->contains($card));
-        $this->assertNotNull($player->getChosenCardSixQP());
 
     }
 
-
+    protected function setUp(): void
+    {
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $this->sixQPService = new SixQPService($entityManager);
+    }
 }
