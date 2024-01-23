1556aeb233086d438407529bec83b648153c007e
bingieti <etienne.binginot@univ-rouen.fr>
2024-01-23 18:58:20 +0100

    AV-49 test: add unit test for cardSixQP

---
+++ tests/Unit/Entity/Game/SixQP/CardSixQPTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Unit\Entity\Game\SixQP;
+
+use App\Entity\Game\SixQP\CardSixQP;
+use PHPUnit\Framework\TestCase;
+
+class CardSixQPTest extends TestCase
+{
+
+    public function testCreateCardWithValidValue()
+    {
+        $card = new CardSixQP();
+        $card->setValue(5);
+        $this->expectNotToPerformAssertions();
+    }
+
+    public function testCreateCardWithInvalidValue()
+    {
+        $card = new CardSixQP();
+        $this->expectException(\Exception::class);
+        $card->setValue(105);
+
+        $card = new CardSixQP();
+        $this->expectException(\Exception::class);
+        $card->setValue(0);
+    }
+
+}
