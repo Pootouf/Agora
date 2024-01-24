cb3e643755dae22a9f4e0f1c2bd1408878f6542b
Etienne Binginot <etienne.binginot@univ-rouen.fr>
2024-01-24 21:50:34 +0100

    AV-52 refactor: refactor code to have a more comprehensible controller

--- src/Controller/Game/SixQPController.php
+++ src/Controller/Game/SixQPController.php
@@ -18,69 +18,31 @@ use Symfony\Component\Security\Core\User\UserInterface;
 class SixQPController extends GameController
 {
 
-    #[Route('/game/{id}/sixqp/select/{id}', name: 'app_game_sixqp_select')]
-    public function selectCard(HubInterface $hub,
-        EntityManagerInterface $entityManager,
-        ChosenCardSixQPRepository $chosenCardSixQPRepository,
-        PlayerSixQPRepository $playerSixQPRepository,
-        SixQPService $service,
-        GameSixQP $game, CardSixQP $card): Response
+    private HubInterface $hub;
+
+    private EntityManagerInterface $entityManager;
+
+    private ChosenCardSixQPRepository $chosenCardSixQPRepository;
+
+    private PlayerSixQPRepository $playerSixQPRepository;
+
+    private SixQPService $service;
+
+    public function __construct(HubInterface $hub, EntityManagerInterface $entityManager,
+                                ChosenCardSixQPRepository $chosenCardSixQPRepository,
+                                PlayerSixQPRepository $playerSixQPRepository,
+                                SixQPService $service)
     {
-        $player = SixQPController::getPlayerFromUser($this->getUser(),
-            $game->getId(),
-            $playerSixQPRepository);
-        if ($player == null) {
-           return $this->redirectToRoute('/');
-        }
-
-        $chosenCard = $chosenCardSixQPRepository->findOneBy(['player'=>$player->getId()]);
-        if ($chosenCard != null) {
-            return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
-        }
-
-        try {
-            $service->chooseCard($player, $card);
-        } catch (\Exception) {
-            return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
-        }
-
-        $response = $this->renderChosenCards($chosenCardSixQPRepository, $game);
-
-        $this->publish($hub, $this->generateUrl('app_game_sixqp_select'), $response);
-
-        $chosenCards = $chosenCardSixQPRepository->findBy(['game' => $game->getId()]);
-        if (count($chosenCards) != count($game->getPlayerSixQPs())) {
-            return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
-        }
-
-        usort($chosenCards, function (ChosenCardSixQP $a, ChosenCardSixQP $b) {
-            return $a->getCard()->getValue() > $b->getCard()->getValue();});
-
-        foreach ($chosenCards as $chosenCard) {
-            $returnValue = $service->placeCard($chosenCard);
-            if ($returnValue == -1) {
-                $this->publish($hub,
-                    $this->generateUrl('app_game_show', ['id' => $game->getId()]).($player->getId()),
-                    new Response());
-                return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
-            } else {
-                $this->publish($hub,
-                    $this->generateUrl('app_game_show', ['id' => $game->getId()]),
-                    $this->renderMainBoard($game));
-            }
-        }
-
-        foreach ($chosenCards as $chosenCard) {
-            $entityManager->remove($chosenCard);
-        }
-        $entityManager->flush();
-
-        return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
+        $this->hub = $hub;
+        $this->entityManager = $entityManager;
+        $this->chosenCardSixQPRepository = $chosenCardSixQPRepository;
+        $this->playerSixQPRepository = $playerSixQPRepository;
+        $this->service = $service;
     }
 
     public static function getPlayerFromUser(?UserInterface $user,
-        int $gameId,
-        PlayerSixQPRepository $playerSixQPRepository): ?PlayerSixQP
+                                             int $gameId,
+                                             PlayerSixQPRepository $playerSixQPRepository): ?PlayerSixQP
     {
         if ($user == null) {
             return null;
@@ -90,10 +52,90 @@ class SixQPController extends GameController
         return $playerSixQPRepository->findOneBy(['id' => $id, 'game' => $gameId]);
     }
 
-    private function renderChosenCards(ChosenCardSixQPRepository $chosenCardSixQPRepository,
-        GameSixQP $gameSixQP): Response
+
+    #[Route('/game/{id}/sixqp/select/{id}', name: 'app_game_sixqp_select')]
+    public function selectCard(GameSixQP $game, CardSixQP $card): Response
     {
-        $cards = $chosenCardSixQPRepository->findBy(['game' => $gameSixQP->getId()]);
+        $player = SixQPController::getPlayerFromUser($this->getUser(),
+            $game->getId(),
+            $this->playerSixQPRepository);
+        if ($player == null) {
+           return $this->redirectToRoute('/');
+        }
+
+        if ($this->doesPlayerAlreadyHavePlay($player)) {
+            return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
+        }
+
+        try {
+            $this->service->chooseCard($player, $card);
+        } catch (\Exception) {
+            return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
+        }
+        $response = $this->renderChosenCards($game);
+        $this->publish($this->hub, $this->generateUrl('app_game_sixqp_select'), $response);
+
+        $chosenCards = $this->chosenCardSixQPRepository->findBy(['game' => $game->getId()]);
+        if (!$this->isEndOfRoundReached($chosenCards, $game)) {
+            return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
+        }
+
+        $response = $this->placeCardAutomatically($game, $player, $chosenCards);
+        if ($response == null) {
+            return $response;
+        }
+
+
+        $this->clearCards($chosenCards);
+        return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
+    }
+
+    private function doesPlayerAlreadyHavePlay(PlayerSixQP $player) {
+        $chosenCard = $this->chosenCardSixQPRepository->findOneBy(['player'=>$player->getId()]);
+        return $chosenCard != null;
+    }
+
+    private function isEndOfRoundReached(array $chosenCards, GameSixQP $game): bool {
+        return count($chosenCards) == count($game->getPlayerSixQPs());
+    }
+
+    private function placeCardAutomatically(GameSixQP $game, PlayerSixQP $player, array $chosenCards): ?Response {
+        usort($chosenCards, function (ChosenCardSixQP $a, ChosenCardSixQP $b) {
+            return $a->getCard()->getValue() > $b->getCard()->getValue();});
+
+        $chosenCards = $this->removeAlreadyPlacedCard($chosenCards, $game);
+
+        foreach ($chosenCards as $chosenCard) {
+            $returnValue = $this->service->placeCard($chosenCard);
+            if ($returnValue == -1) {
+                $this->publish($this->hub,
+                    $this->generateUrl('app_game_show', ['id' => $game->getId()]).($player->getId()),
+                    new Response());
+                return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
+            } else {
+                $this->publish($this->hub,
+                    $this->generateUrl('app_game_show', ['id' => $game->getId()]),
+                    $this->renderMainBoard($game));
+            }
+        }
+
+        return null;
+    }
+
+    private function removeAlreadyPlacedCard(array $chosenCards, GameSixQP $game): array {
+        for ($i = 0; $i < count($chosenCards); $i++) {
+            foreach ($game->getRowSixQPs() as $row) {
+                if ($row->isCardInRow($chosenCards[$i]->getCard())) {
+                    array_splice($chosenCards, $i, 1);
+                }
+            }
+        }
+        return $chosenCards;
+    }
+
+    private function renderChosenCards(GameSixQP $gameSixQP): Response
+    {
+        $cards = $this->chosenCardSixQPRepository->findBy(['game' => $gameSixQP->getId()]);
         return $this->render('Game/Six_qp/chosenCards.html.twig',
             ['chosenCards' => $cards]
         );
@@ -103,5 +145,13 @@ class SixQPController extends GameController
     {
         return $this->render('Game/Six_qp/mainBoard.html.twig',
             ['rows' => $gameSixQP->getRowSixQPs()]);
+    }
+
+    private function clearCards(array $chosenCards): void
+    {
+        foreach ($chosenCards as $chosenCard) {
+            $this->entityManager->remove($chosenCard);
+        }
+        $this->entityManager->flush();
     }
 }
