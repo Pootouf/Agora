b583e5349fee7ca8ebcab28fe616372717b4865c
bingieti <etienne.binginot@univ-rouen.fr>
2024-01-24 19:50:40 +0100

    AV-52 feature: place automatically card at the end of the round

--- src/Controller/Game/SixQPController.php
+++ src/Controller/Game/SixQPController.php
@@ -3,11 +3,13 @@
 namespace App\Controller\Game;
 
 use App\Entity\Game\SixQP\CardSixQP;
+use App\Entity\Game\SixQP\ChosenCardSixQP;
 use App\Entity\Game\SixQP\GameSixQP;
 use App\Entity\Game\SixQP\PlayerSixQP;
 use App\Repository\Game\SixQP\ChosenCardSixQPRepository;
 use App\Repository\Game\SixQP\PlayerSixQPRepository;
 use App\Service\Game\SixQPService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mercure\HubInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,6 +20,7 @@ class SixQPController extends GameController
 
     #[Route('/game/{id}/sixqp/select/{id}', name: 'app_game_sixqp_select')]
     public function selectCard(HubInterface $hub,
+        EntityManagerInterface $entityManager,
         ChosenCardSixQPRepository $chosenCardSixQPRepository,
         PlayerSixQPRepository $playerSixQPRepository,
         SixQPService $service,
@@ -30,6 +33,11 @@ class SixQPController extends GameController
            return $this->redirectToRoute('/');
         }
 
+        $chosenCard = $chosenCardSixQPRepository->findOneBy(['player'=>$player->getId()]);
+        if ($chosenCard != null) {
+            return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
+        }
+
         try {
             $service->chooseCard($player, $card);
         } catch (\Exception) {
@@ -39,6 +47,33 @@ class SixQPController extends GameController
         $response = $this->renderChosenCards($chosenCardSixQPRepository, $game);
 
         $this->publish($hub, $this->generateUrl('app_game_sixqp_select'), $response);
+
+        $chosenCards = $chosenCardSixQPRepository->findBy(['game' => $game->getId()]);
+        if (count($chosenCards) != count($game->getPlayerSixQPs())) {
+            return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
+        }
+
+        usort($chosenCards, function (ChosenCardSixQP $a, ChosenCardSixQP $b) {
+            return $a->getCard()->getValue() > $b->getCard()->getValue();});
+
+        foreach ($chosenCards as $chosenCard) {
+            $returnValue = $service->placeCard($chosenCard);
+            if ($returnValue == -1) {
+                $this->publish($hub,
+                    $this->generateUrl('app_game_show', ['id' => $game->getId()]).($player->getId()),
+                    new Response());
+                return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
+            } else {
+                $this->publish($hub,
+                    $this->generateUrl('app_game_show', ['id' => $game->getId()]),
+                    $this->renderMainBoard($game));
+            }
+        }
+
+        foreach ($chosenCards as $chosenCard) {
+            $entityManager->remove($chosenCard);
+        }
+        $entityManager->flush();
 
         return $this->redirectToRoute('app_game_show', ['id'=>$game->getId()]);
     }
@@ -62,5 +97,11 @@ class SixQPController extends GameController
         return $this->render('Game/Six_qp/chosenCards.html.twig',
             ['chosenCards' => $cards]
         );
+    }
+
+    private function renderMainBoard(GameSixQP $gameSixQP): Response
+    {
+        return $this->render('Game/Six_qp/mainBoard.html.twig',
+            ['rows' => $gameSixQP->getRowSixQPs()]);
     }
 }
