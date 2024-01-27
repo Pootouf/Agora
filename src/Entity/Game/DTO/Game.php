aeb1f70dacea8d7fd554422a7391e45f97c56e8b
Etienne Binginot <etienne.binginot@univ-rouen.fr>
2024-01-27 19:14:10 +0100

    AV-61 feature: add launched property to game

--- src/Entity/Game/DTO/Game.php
+++ src/Entity/Game/DTO/Game.php
@@ -14,9 +14,35 @@ class Game
     #[ORM\Column]
     protected ?int $id = null;
 
+    #[ORM\Column]
+    protected bool $launched = false;
+
+    #[ORM\Column]
+    protected ?string $gameName = null;
+
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function isLaunched(): ?bool
+    {
+        return $this->launched;
+    }
+
+    public function setLaunched(?bool $launched): void
+    {
+        $this->launched = $launched;
+    }
+
+    public function getGameName(): ?string
+    {
+        return $this->gameName;
+    }
+
+    public function setGameName(?string $gameName): void
+    {
+        $this->gameName = $gameName;
     }
 }
