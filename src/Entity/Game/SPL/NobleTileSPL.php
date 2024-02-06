2cfb505bcbadf1e6d6ebef76c5a6839948af1ccc
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-02-06 15:34:48 +0100

    AV-119 Feature : created NobleTileSPL entity

--- src/Entity/Game/SPL/NobleTileSPL.php
+++ src/Entity/Game/SPL/NobleTileSPL.php
@@ -3,6 +3,7 @@
 namespace App\Entity\Game\SPL;
 
 use App\Repository\Game\SPL\NobleTileSPLRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: NobleTileSPLRepository::class)]
@@ -13,8 +14,53 @@ class NobleTileSPL
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column]
+    private ?int $prestigePoints = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $costCardsColor = [];
+
+    #[ORM\Column(type: Types::ARRAY)]
+    private array $countCostCardsColor = [];
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getPrestigePoints(): ?int
+    {
+        return $this->prestigePoints;
+    }
+
+    public function setPrestigePoints(int $prestigePoints): static
+    {
+        $this->prestigePoints = $prestigePoints;
+
+        return $this;
+    }
+
+    public function getCostCardsColor(): array
+    {
+        return $this->costCardsColor;
+    }
+
+    public function setCostCardsColor(array $costCardsColor): static
+    {
+        $this->costCardsColor = $costCardsColor;
+
+        return $this;
+    }
+
+    public function getCountCostCardsColor(): array
+    {
+        return $this->countCostCardsColor;
+    }
+
+    public function setCountCostCardsColor(array $countCostCardsColor): static
+    {
+        $this->countCostCardsColor = $countCostCardsColor;
+
+        return $this;
     }
 }
