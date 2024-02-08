76d431291b56c224c97f672cdd4ea2ffec48d042
MisterGit <anass.elgharbaoui@usmba.ac.ma>
2024-02-08 17:16:55 +0100

    create user controllers

---
+++ src/Controller/Platform/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Platform;
+
+use App\Entity\Platform\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user/{id}', name:'app_userbyid')]
+    public function sendUserInfo(int $id, EntityManagerInterface $entityManager): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED');
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        if(!$user){
+            throw $this->createNotFoundException(
+                "user not found".$id);
+        }
+
+        return new Response(
+        );
+    }
+
+    #[Route('/user/edit/{id}', name:'app_editUser')]
+    public function editUser(int $id, EntityManagerInterface $entityManager) :Response {
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        if(!$user){
+            throw $this->createNotFoundException(
+                "user not found".$id);
+        }
+
+        return new Response();
+    }
+
+
+
+
+}
