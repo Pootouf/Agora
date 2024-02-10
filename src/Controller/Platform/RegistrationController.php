2f72e9bdfe7649760bc8dfb192b44850d36ca9be
Sid Ahmed BRAHIMI <brahimi.sdhmd@gmail.com>
2024-02-10 19:46:47 +0100

    fix: redirect when authenticated

--- src/Controller/Platform/RegistrationController.php
+++ src/Controller/Platform/RegistrationController.php
@@ -28,6 +28,10 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+        if($this->getUser()) {
+            return $this->redirect('/');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
