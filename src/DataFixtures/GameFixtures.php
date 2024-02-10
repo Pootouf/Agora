92669d062afd9b5421306e84eda311c0b2b1bfdd
lehucher <robin.lehucher@univ-rouen.fr>
2024-02-10 17:17:38 +0100

    feature : create GameFixtures class

---
+++ src/DataFixtures/GameFixtures.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Platform\Game;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class GameFixtures extends Fixture
+{
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i=1; $i <= 50; $i++) {
+            $game = new Game();
+            $game->setName("gameN°". $i);
+            $game->setDescrRule("Les règles du jeu blablabla");
+            $game->setDescrRule("No image now");
+            $game->setLabel("G".$i);
+            $game->setMinPlayers(rand(2,4));
+            $game->setMaxPlayers(rand(5,10));
+            $game->setIsActive(true);
+            $manager->persist($game);
+        }
+        $manager->flush();
+    }
+
+}
