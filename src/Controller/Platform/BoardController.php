5164b05a39003aa6d3c3ca35a72680e03e4870a3
Corentin PILLE <corentin.pille@univ-rouen.fr>
2024-02-10 21:24:25 +0100

    Formulaire de création de table (statique)

---
+++ src/Controller/Platform/BoardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\Platform;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BoardController extends AbstractController
+{
+    #[Route('/newBoard')]
+    public function index(): Response
+    {
+        return $this->render('platform/board/newBoard.html.twig');
+    }
+}
