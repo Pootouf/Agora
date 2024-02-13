6577865ec59194ed790b9ec9b3f86b8665883fd5
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-02-13 14:22:06 +0100

    test to check if player owns card

--- tests/Game/SixQP/Application/SixQPControllerTest.php
+++ tests/Game/SixQP/Application/SixQPControllerTest.php
@@ -8,6 +8,7 @@ use App\Repository\Game\GameUserRepository;
 use App\Repository\Game\SixQP\GameSixQPRepository;
 use App\Repository\Game\SixQP\PlayerSixQPRepository;
 use App\Service\Game\SixQP\SixQPGameManagerService;
+use App\Service\Game\SixQP\SixQPService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -25,7 +26,7 @@ class SixQPControllerTest extends WebTestCase
 
     private KernelBrowser $client3;
 
-
+    private SixQPService $sixQPService;
     public function testPlayersHaveAccessToGame() : void
     {
         $gameId = $this->initializeGameWithTwoPlayers();
@@ -54,7 +55,8 @@ class SixQPControllerTest extends WebTestCase
         $gameId = $this->initializeGameWithTwoPlayers();
         $url = "/game/sixqp/" . $gameId;
         $this->client1->request("GET", $url);
-        $player = $this->playerSixQPRepository->findOneBy(['game' => $gameId, 'username' => "test0"]);
+        $game = $this->gameSixQPRepository->findOneBy(["id" => $gameId]);
+        $player = $this->sixQPService->getPlayerFromNameAndGame($game, "test0");
         $card = $player->getCards()[0];
         $card2 = $player->getCards()[1];
         $user1 = $this->gameUserRepository->findOneByUsername("test0");
@@ -119,6 +121,7 @@ class SixQPControllerTest extends WebTestCase
         $this->gameUserRepository = static::getContainer()->get(GameUserRepository::class);
         $this->gameSixQPRepository = static::getContainer()->get(GameSixQPRepository::class);
         $this->playerSixQPRepository = static::getContainer()->get(PlayerSixQPRepository::class);
+        $this->sixQPService = static::getContainer()->get(SixQPService::class);
         $user1 = $this->gameUserRepository->findOneByUsername("test0");
         $user2 = $this->gameUserRepository->findOneByUsername("test1");
         $user3 = $this->gameUserRepository->findOneByUsername("test2");
