ce56958f6e43fdb1c6fc7037ba00140443a2b1d3
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-02-15 21:19:00 +0100

    Feature: Finished all tests on 6QP entities

--- tests/Game/SixQP/Unit/Entity/CardSixQPTest.php
+++ tests/Game/SixQP/Unit/Entity/CardSixQPTest.php
@@ -7,22 +7,36 @@ use PHPUnit\Framework\TestCase;
 
 class CardSixQPTest extends TestCase
 {
-
-    public function testCreateCardWithValidValue()
+    public function testCreateCardWithValidValue() : void
     {
+        //GIVEN
+
+        //WHEN
         $card = new CardSixQP();
         $card->setValue(5);
-        $this->expectNotToPerformAssertions();
+        $card->setPoints(12);
+
+        //THEN
+        $this->assertSame(5, $card->getValue());
+        $this->assertSame(12, $card->getPoints());
     }
 
-    public function testCreateCardWithInvalidValue()
+    public function testCreateCardWithInvalidValueWhenTooHigh() : void
     {
+        //GIVEN
         $card = new CardSixQP();
+        //THEN
         $this->expectException(\Exception::class);
+        //WHEN
         $card->setValue(105);
-
+    }
+    public function testCreateCardWithInvalidValueWhenTooLow()
+    {
+        //GIVEN
         $card = new CardSixQP();
+        //THEN
         $this->expectException(\Exception::class);
+        //WHEN
         $card->setValue(0);
     }
 
