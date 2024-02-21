caa3465ed1a82229f80be63e45c22b956048d1cb
Mohamed Cherfi <mohamed.cherfi@univ-rouen.fr>
2024-02-21 20:40:12 +0100

    visiteur pas de boutons, connecté boutons dispo

--- src/Controller/Platform/TableController.php
+++ src/Controller/Platform/TableController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Platform;
 
+use Symfony\Bundle\SecurityBundle\Security;
 use App\Form\Platform\SearchBoardType;
 use App\Repository\Platform\BoardRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,8 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class TableController extends AbstractController
 {
     #[Route('/table', name: 'app_table', methods: ['GET'])]
-    public function index(Request $request, BoardRepository $boardRepository): Response
+    public function index(Security $security, Request $request, BoardRepository $boardRepository): Response
     {
+        $currentUserID = null;
+        if ($security->getUser() !== null) {
+            $currentUserID = $security->getUser()->getId();
+        }
+
         $boards = $boardRepository->findAll();
         $form = $this->createForm(SearchBoardType::class);
         $form->handleRequest($request);
@@ -21,6 +27,7 @@ class TableController extends AbstractController
         return $this->render('platform/table/table.html.twig', [
             'boards' => $boards,
             'searchboard' => $form->createView(),
+            'currentUserID' => $currentUserID,
             'controller_name' => 'TableController',
         ]);
     }
