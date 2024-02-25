7e601c186d1ac8a1983945eaecb84e81a03decac
Sid Ahmed BRAHIMI <brahimi.sdhmd@gmail.com>
2024-02-25 22:01:21 +0100

    fix: add dyanmic game description page by id

--- src/Controller/Platform/DescriptionController.php
+++ src/Controller/Platform/DescriptionController.php
@@ -1,20 +1,27 @@
 <?php
 
 namespace App\Controller\Platform;
+use Doctrine\ORM\EntityManagerInterface;
 
+use App\Entity\Platform\Game;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DescriptionController extends AbstractController
 {
-    #[Route('/description', name: 'app_desc')]
-    public function index(int $id): Response
+    #[Route('/description/{game_id}', name: 'app_game_description', requirements: ['game_id' => '\d+'], methods: ['GET', 'HEAD'])]
+    public function index(EntityManagerInterface $entityManager, int $game_id): Response
     {
+        $gameRepository = $entityManager->getRepository(Game::class);
+        $game = $gameRepository->find($game_id);
 
+        if(!$game) {
+            throw $this->createNotFoundException('Le jeu n\'existe pas');
+        }
 
         return $this->render('platform/game/description.html.twig', [
-            'controller_name' => 'DescriptionController',
+            'game' => $game,
         ]);
     }
 }
