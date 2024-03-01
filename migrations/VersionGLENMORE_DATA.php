8729d57d9516fca52a9d5abdbd458486e4bafc83
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-03-01 15:37:38 +0100

    Feature: modified Glenmore migration for cards and level 0 tiles after modification of entities

--- migrations/VersionGLENMORE_DATA.php
+++ migrations/VersionGLENMORE_DATA.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace DoctrineMigrations;
 
@@ -57,7 +57,7 @@ final class VersionGLENMORE_DATA extends AbstractMigration
 
         // Insertion of card 1
 
-   /*     $this->connection->insert('tile_bonus_glm',
+        $this->connection->insert('tile_buy_bonus_glm',
             ['id' => 1, 'help_id' => null, 'amount' => 1, 'resource_id' => 7]
         );
 
@@ -76,7 +76,7 @@ final class VersionGLENMORE_DATA extends AbstractMigration
 
         // Insertion of card 3
 
-        $this->connection->insert('tile_bonus_glm',
+        $this->connection->insert('tile_buy_bonus_glm',
             ['id' => 3, 'help_id' => null, 'amount' => 1, 'resource_id' => 7]
         );
 
@@ -88,7 +88,7 @@ final class VersionGLENMORE_DATA extends AbstractMigration
 
         // Insertion of card 4
 
-        $this->connection->insert('tile_bonus_glm',
+        $this->connection->insert('tile_buy_bonus_glm',
             ['id' => 2, 'help_id' => null, 'amount' => 3, 'resource_id' => 7]
         );
 
@@ -127,7 +127,7 @@ final class VersionGLENMORE_DATA extends AbstractMigration
 
         // Insertion of card 9
 
-        $this->connection->insert('tile_bonus_glm',
+        $this->connection->insert('tile_buy_bonus_glm',
             ['id' => 4, 'help_id' => null, 'amount' => 1, 'resource_id' => 7]
         );
 
@@ -145,7 +145,7 @@ final class VersionGLENMORE_DATA extends AbstractMigration
 
         // Insertion of card 11
 
-        $this->connection->insert('tile_bonus_glm',
+        $this->connection->insert('tile_buy_bonus_glm',
             ['id' => 5, 'help_id' => null, 'amount' => 1, 'resource_id' => 7]
         );
 
@@ -163,14 +163,14 @@ final class VersionGLENMORE_DATA extends AbstractMigration
 
         // Insertion of card 13
 
-        $this->connection->insert('tile_bonus_glm',
+        $this->connection->insert('tile_buy_bonus_glm',
             ['id' => 6, 'help_id' => null, 'amount' => 1, 'resource_id' => 7]
         );
 
         $this->connection->insert('card_glm',
             ['id' => 13, 'help_id' => null, 'bonus_id' => 6,
                 'value' => null, 'name' => GlenmoreParameters::$CARD_ARMADALE_CASTLE]
-        );*/
+        );
 
         // Insertion of tiles
 
@@ -178,192 +178,180 @@ final class VersionGLENMORE_DATA extends AbstractMigration
 
         // Insertion of tile 1
 
-    /*    $this->connection->insert('tile_bonus_glm',
-            ['id' => 7, "help_id" => null, 'amount' => 1, 'resource_id' => 2]
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 1, "help_id" => null, 'amount' => 1, 'resource_id' => 2]
         );
 
         $this->connection->insert('tile_glm',
-        ['id' => 1, 'help_id' => null,
-            'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_YELLOW,
-            'name' => GlenmoreParameters::$TILE_NAME_QUARRY, 'containing_river' => 0,
-            'containing_road' => 1, 'level' => 0]);
-*/
+            ['id' => 1, 'help_id' => null,
+                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_YELLOW,
+                'name' => GlenmoreParameters::$TILE_NAME_QUARRY, 'containing_river' => 0,
+                'containing_road' => 1, 'level' => 0]);
+
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 1, 'tile_activation_bonus_glm_id' => 1]);
+
         // Insertion of tile 2
 
-///       $this->connection->insert('tile_bonus_glm',
-   //         ['id' => 8, "help_id" => null, 'amount' => 1]
-     //   );
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 2, "help_id" => null, 'amount' => 1, 'resource_id' => 1]
+        );
 
-       // $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 8, 'resource_glm_id' => 1]
-//        );
-//
-//        $this->connection->insert('tile_glm',
-//            ['id' => 2, 'help_id' => null, 'buy_bonus_id' => null, 'activation_bonus_id' => 8,
-//                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_YELLOW,
-//                'name' => GlenmoreParameters::$TILE_NAME_FOREST, 'containing_river' => 1,
-//                'containing_road' => 0, 'level' => 0]);
-//
-//        // Insertion of tile 3
-//
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 9, "help_id" => null, 'amount' => 1]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 9, 'resource_glm_id' => 3]
-//        );
-//
-//        $this->connection->insert('tile_glm',
-//            ['id' => 3, 'help_id' => null, 'buy_bonus_id' => null, 'activation_bonus_id' => 9,
-//                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_GREEN,
-//                'name' => GlenmoreParameters::$TILE_NAME_PASTURE, 'containing_river' => 1,
-//                'containing_road' => 0, 'level' => 0]);
-//
-//        // Insertion of tile 4
-//
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 10, "help_id" => null, 'amount' => 1]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 10, 'resource_glm_id' => 8]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 11, "help_id" => null, 'amount' => 1]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 11, 'resource_glm_id' => 9]
-//        );
-//
-//        $this->connection->insert('tile_glm',
-//            ['id' => 4, 'help_id' => null, 'buy_bonus_id' => 11, 'activation_bonus_id' => 10,
-//                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_VILLAGE,
-//                'name' => GlenmoreParameters::$TILE_NAME_VILLAGE, 'containing_river' => 0,
-//                'containing_road' => 1, 'level' => 0]);
-//
-//        // Insertion of tile 5
-//
-//
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 12, "help_id" => null, 'amount' => 1]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 12, 'resource_glm_id' => 5]
-//        );
-//
-//        $this->connection->insert('tile_glm',
-//            ['id' => 5, 'help_id' => null, 'buy_bonus_id' => null, 'activation_bonus_id' => 12,
-//                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_GREEN,
-//                'name' => GlenmoreParameters::$TILE_NAME_FIELD, 'containing_river' => 0,
-//                'containing_road' => 1, 'level' => 0]);
-//
-//        // Insertion of tile 6
-//
-//
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 13, "help_id" => null, 'amount' => 1]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 13, 'resource_glm_id' => 4]
-//        );
-//
-//        $this->connection->insert('tile_glm',
-//            ['id' => 6, 'help_id' => null, 'buy_bonus_id' => null, 'activation_bonus_id' => 13,
-//                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_GREEN,
-//                'name' => GlenmoreParameters::$TILE_NAME_CATTLE, 'containing_river' => 1,
-//                'containing_road' => 0, 'level' => 0]);
-//
-//        // Insertion of tile 7
-//
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 14, "help_id" => null, 'amount' => 1]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 14, 'resource_glm_id' => 2]
-//        );
-//
-//        $this->connection->insert('tile_glm',
-//            ['id' => 7, 'help_id' => null, 'buy_bonus_id' => null, 'activation_bonus_id' => 14,
-//                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_YELLOW,
-//                'name' => GlenmoreParameters::$TILE_NAME_QUARRY, 'containing_river' => 1,
-//                'containing_road' => 0, 'level' => 0]);
-//
-//        // Insertion of tile 8
-//
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 15, "help_id" => null, 'amount' => 1]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 15, 'resource_glm_id' => 1]
-//        );
-//
-//        $this->connection->insert('tile_glm',
-//            ['id' => 8, 'help_id' => null, 'buy_bonus_id' => null, 'activation_bonus_id' => 15,
-//                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_YELLOW,
-//                'name' => GlenmoreParameters::$TILE_NAME_FOREST, 'containing_river' => 0,
-//                'containing_road' => 1, 'level' => 0]);
+        $this->connection->insert('tile_glm',
+            ['id' => 2, 'help_id' => null,
+                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_YELLOW,
+                'name' => GlenmoreParameters::$TILE_NAME_FOREST, 'containing_river' => 1,
+                'containing_road' => 0, 'level' => 0]);
+
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 2, 'tile_activation_bonus_glm_id' => 2]);
+
+        // Insertion of tile 3
+
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 3, "help_id" => null, 'amount' => 1, 'resource_id' => 3]
+        );
+
+        $this->connection->insert('tile_glm',
+            ['id' => 3, 'help_id' => null,
+                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_GREEN,
+                'name' => GlenmoreParameters::$TILE_NAME_PASTURE, 'containing_river' => 1,
+                'containing_road' => 0, 'level' => 0]);
+
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 3, 'tile_activation_bonus_glm_id' => 3]);
+
+        // Insertion of tile 4
+
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 4, "help_id" => null, 'amount' => 1, 'resource_id' => 8]
+        );
+
+        $this->connection->insert('tile_buy_bonus_glm',
+            ['id' => 7, "help_id" => null, 'amount' => 1, 'resource_id' => 9]
+        );
+
+        $this->connection->insert('tile_glm',
+            ['id' => 4, 'help_id' => null,
+                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_VILLAGE,
+                'name' => GlenmoreParameters::$TILE_NAME_VILLAGE, 'containing_river' => 0,
+                'containing_road' => 1, 'level' => 0]);
+
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 4, 'tile_activation_bonus_glm_id' => 4]);
+
+        $this->connection->insert('tile_glm_tile_buy_bonus_glm',
+            ['tile_glm_id' => 4, 'tile_buy_bonus_glm_id' => 7]);
+
+        // Insertion of tile 5
+
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 5, "help_id" => null, 'amount' => 1, 'resource_id' => 6]
+        );
+
+        $this->connection->insert('tile_glm',
+            ['id' => 5, 'help_id' => null,
+                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_GREEN,
+                'name' => GlenmoreParameters::$TILE_NAME_FIELD, 'containing_river' => 0,
+                'containing_road' => 1, 'level' => 0]);
+
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 5, 'tile_activation_bonus_glm_id' => 5]);
+
+        // Insertion of tile 6
+
+
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 6, "help_id" => null, 'amount' => 1, 'resource_id' => 4]
+        );
+
+        $this->connection->insert('tile_glm',
+            ['id' => 6, 'help_id' => null,
+                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_GREEN,
+                'name' => GlenmoreParameters::$TILE_NAME_CATTLE, 'containing_river' => 1,
+                'containing_road' => 0, 'level' => 0]);
+
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 6, 'tile_activation_bonus_glm_id' => 6]);
+
+        // Insertion of tile 7
+
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 7, "help_id" => null, 'amount' => 1, 'resource_id' => 1]
+        );
+
+        $this->connection->insert('tile_glm',
+            ['id' => 7, 'help_id' => null,
+                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_YELLOW,
+                'name' => GlenmoreParameters::$TILE_NAME_QUARRY, 'containing_river' => 1,
+                'containing_road' => 0, 'level' => 0]);
+
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 7, 'tile_activation_bonus_glm_id' => 7]);
+
+        // Insertion of tile 8
+
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 8, "help_id" => null, 'amount' => 1, 'resource_id' => 1]
+        );
+
+
+        $this->connection->insert('tile_glm',
+            ['id' => 8, 'help_id' => null,
+                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_YELLOW,
+                'name' => GlenmoreParameters::$TILE_NAME_FOREST, 'containing_river' => 0,
+                'containing_road' => 1, 'level' => 0]);
+
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 8, 'tile_activation_bonus_glm_id' => 8]);
 
         // Insertion of tiles level 1
 
         // Insertion of tile 9
 
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 16, "help_id" => null, 'amount' => 1]
-//        );
-//
-//        $this->connection->insert('tile_glm',
-//            ['id' => 9, 'help_id' => null, 'buy_bonus_id' => null, 'activation_bonus_id' => 16,
-//                'card_id' => 2, 'type' => GlenmoreParameters::$TILE_TYPE_BLUE,
-//                'name' => GlenmoreParameters::$CARD_LOCH_LOCHY, 'containing_river' => 1,
-//                'containing_road' => 0, 'level' => 1]);
+        $this->connection->insert('tile_glm',
+            ['id' => 9, 'help_id' => null,
+                'card_id' => 2, 'type' => GlenmoreParameters::$TILE_TYPE_BLUE,
+                'name' => GlenmoreParameters::$CARD_LOCH_LOCHY, 'containing_river' => 1,
+                'containing_road' => 0, 'level' => 1]);
 
         // Insertion of tile 10
 
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 17, "help_id" => null, 'amount' => 2]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 17, 'resource_glm_id' => 10]
-//        );
+        $this->connection->insert('tile_activation_cost_glm',
+            ['id' => 1, "help_id" => null, 'price' => 1, 'resource_id' => 3]
+        );
 
-//        $this->connection->insert('tile_bonus_glm',
-//            ['id' => 17, "help_id" => null, 'amount' => 4]
-//        );
-//
-//        $this->connection->insert('tile_bonus_glm_resource_glm',
-//            ['tile_bonus_glm_id' => 17, 'resource_glm_id' => 10]
-//        );
-//
-//        $this->connection->insert('tile_glm',
-//            ['id' => 10, 'help_id' => null, 'buy_bonus_id' => null, 'activation_bonus_id' => 17,
-//                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_BROWN,
-//                'name' => GlenmoreParameters::$TILE_NAME_BUTCHER, 'containing_river' => 0,
-//                'containing_road' => 0, 'level' => 1]);
-//
-//        $this->connection->insert('tile_cost_glm',
-//            ['id' => 1, 'help_id' => null, 'tile_glm_id' => 10, 'tile_bonus_glm_id' => 16, 'price' => 1]
-//        );
-//
-//        $this->connection->insert('tile_cost_glm_resource_glm',
-//            ['tile_cost_glm_id' => 1, 'resource_glm_id' => 3]
-//        );
-//
-//        $this->connection->insert('tile_cost_glm',
-//            ['id' => 2, 'help_id' => null, 'tile_glm_id' => 10, 'tile_bonus_glm_id' => 17, 'price' => 2]
-//        );
-//
-//        $this->connection->insert('tile_cost_glm_resource_glm',
-//            ['tile_cost_glm_id' => 2, 'resource_glm_id' => 3]
-//        );
+        $this->connection->insert('tile_activation_cost_glm',
+            ['id' => 2, "help_id" => null, 'price' => 2, 'resource_id' => 3]
+        );
 
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 9, "help_id" => null, 'amount' => 2, 'resource_id' => 10]
+        );
+
+        $this->connection->insert('tile_activation_bonus_glm',
+            ['id' => 10, "help_id" => null, 'amount' => 4, 'resource_id' => 10]
+        );
+
+        $this->connection->insert('tile_glm',
+            ['id' => 10, 'help_id' => null,
+                'card_id' => null, 'type' => GlenmoreParameters::$TILE_TYPE_BROWN,
+                'name' => GlenmoreParameters::$TILE_NAME_BUTCHER, 'containing_river' => 0,
+                'containing_road' => 0, 'level' => 1]);
+
+        $this->connection->insert('tile_glm_tile_activation_cost_glm',
+            ['tile_glm_id' => 10, 'tile_activation_cost_glm_id' => 1]
+        );
+        $this->connection->insert('tile_glm_tile_activation_cost_glm',
+            ['tile_glm_id' => 10, 'tile_activation_cost_glm_id' => 2]
+        );
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 10, 'tile_activation_bonus_glm_id' => 9]
+        );
+
+        $this->connection->insert('tile_glm_tile_activation_bonus_glm',
+            ['tile_glm_id' => 10, 'tile_activation_bonus_glm_id' => 10]
+        );
 
     }
 
