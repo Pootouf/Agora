cf24098ccbd2a56e2499d8605d95aceecb4766f2
ellexart <axelle.mazuy@gmail.com>
2024-03-03 14:50:27 +0100

    AV-234 feature: add an openable personal board, add message bar, add stimulus controller for Glenmore for opening personal board

--- app/assets/controllers/glenmore_controller.js
+++ app/assets/controllers/glenmore_controller.js
@@ -10,5 +10,35 @@ import { Controller } from '@hotwired/stimulus';
  * Delete this file or adapt it for your use!
  */
 export default class extends Controller {
+    togglePersonalBoard(isOpening) {
+        const open = isOpening.params.open;
+        const openedPersonalBoard = document.getElementById("openedPersonalBoard");
+        const closedPersonalBoard = document.getElementById("closedPersonalBoard");
+        const Timing = {
+            duration: 600,
+            iterations: 1,
+        }
+        if (open) {
+            const hidden = document.createAttribute("hidden");
+            closedPersonalBoard.setAttributeNode(hidden);
+            openedPersonalBoard.removeAttribute("hidden");
+            const openingSliding = [
+                { transform: "translateY(40rem)"},
+                { transform: "translateY(0rem)"}
+            ]
+            openedPersonalBoard.animate(openingSliding,Timing);
 
+            let personalBoard = document.getElementById('tileBoard');
+            personalBoard.scrollTop = personalBoard.scrollHeight;
+        } else {
+            const hidden = document.createAttribute("hidden");
+            closedPersonalBoard.removeAttribute("hidden");
+            const closingSliding = [
+                { transform: "translateY(0rem)"},
+                { transform: "translateY(40rem)"}
+            ]
+            openedPersonalBoard.animate(closingSliding,Timing).addEventListener("finish",
+                () => openedPersonalBoard.setAttributeNode(hidden));
+        }
+    }
 }
