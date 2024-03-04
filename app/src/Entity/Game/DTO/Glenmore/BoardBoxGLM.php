ef6dfefd667cf6e9f41b98d45c830627cb289319
ellexart <axelle.mazuy@gmail.com>
2024-03-04 12:35:38 +0100

    AV-238 feature: creation of a new DTO for board's box in Glenmore

---
+++ app/src/Entity/Game/DTO/Glenmore/BoardBoxGLM.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity\Game\DTO\Glenmore;
+
+use App\Entity\Game\Glenmore\PawnGLM;
+use App\Entity\Game\Glenmore\TileGLM;
+use Exception;
+use phpDocumentor\Reflection\Types\Boolean;
+use PHPUnit\Framework\InvalidArgumentException;
+
+/**
+ * A representation of board's box for the main board of Glenmore.
+ * A box can contain a tile or a pawn, but can't contain the two at the same time.
+ */
+class BoardBoxGLM
+{
+
+    private ?PawnGLM $pawn;
+    private ?TileGLM $tile;
+
+    /**
+     * @throws Exception
+     */
+    public function __construct(?PawnGLM $pawn, ?TileGLM $tile)
+    {
+        if(($pawn == null && $tile == null) || ($pawn != null && $tile != null)) {
+            throw new Exception("Wrong parameters number");
+        }
+        if($pawn != null) {
+            $this->pawn = $pawn;
+        } else {
+            $this->tile = $tile;
+        }
+    }
+
+    /**
+     * hasTile : indicate if the board's box has a tile
+     * @return bool
+     */
+    public function hasTile() : Boolean
+    {
+        return $this->tile != null;
+    }
+
+    /**
+     * hasPawn : indicate if the board's box has a pawn
+     * @return bool
+     */
+    public function hasPawn() : Boolean
+    {
+        return $this->pawn != null;
+    }
+
+    /**
+     * getTile : return the tile presents on the board's box
+     * @return TileGLM
+     */
+    public function getTile() : TileGLM
+    {
+        return $this->tile;
+    }
+
+    /**
+     * getPawn : return the pawn presents on the board's box
+     * @return PawnGLM
+     */
+    public function getPawn() : PawnGLM
+    {
+        return $this->pawn;
+    }
+}
