94b2b8b78aeb3dc658ab643d50c76547df5d09fa
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-03-05 10:57:14 +0100

    Feature: started to create methods for cards effects after buying

--- app/src/Service/Game/Glenmore/CardGLMService.php
+++ app/src/Service/Game/Glenmore/CardGLMService.php
@@ -5,21 +5,27 @@ namespace App\Service\Game\Glenmore;
 use App\Entity\Game\Glenmore\GameGLM;
 use App\Entity\Game\Glenmore\GlenmoreParameters;
 use App\Entity\Game\Glenmore\PersonalBoardGLM;
+use App\Entity\Game\Glenmore\PlayerTileGLM;
+use App\Entity\Game\Glenmore\PlayerTileResourceGLM;
+use App\Entity\Game\Glenmore\ResourceGLM;
+use App\Entity\Game\Glenmore\TileBuyBonusGLM;
+use App\Repository\Game\Glenmore\ResourceGLMRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class CardGLMService
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
-    {}
+    public function __construct(private readonly EntityManagerInterface $entityManager,
+        private ResourceGLMRepository $resourceGLMRepository) {}
 
     /** applyCastle Of Mey : applies effect of card Castle Of Mey
+     *
      * @param PersonalBoardGLM $personalBoard
      * @param int              $playerResource
      * @return int
      */
-    public function applyCastleOfMey(PersonalBoardGLM $personalBoard, int $playerResource) : int
+    public function applyCastleOfMey(PersonalBoardGLM $personalBoard, int $playerResource): int
     {
-        foreach($personalBoard->getPlayerCardGLM() as $playerCard) {
+        foreach ($personalBoard->getPlayerCardGLM() as $playerCard) {
             if ($playerCard->getCard()->getName() == GlenmoreParameters::$CARD_CASTLE_OF_MEY) {
                 $playerResource *= 2;
                 break;
@@ -29,11 +35,49 @@ class CardGLMService
     }
 
     /**
+     * buyCardManagement : applies effect of the card associated to playerTile
+     * @param PlayerTileGLM $playerTileGLM
+     * @return void
+     */
+    public function buyCardManagement(PlayerTileGLM $playerTileGLM) : void
+    {
+        $tile = $playerTileGLM->getTile();
+        $personalBoard = $playerTileGLM->getPersonalBoard();
+        $card = $tile->getCard();
+        switch($card->getName()) {
+            case GlenmoreParameters::$CARD_CASTLE_STALKER:
+                $this->applyCastleStalker($playerTileGLM);
+                break;
+            case GlenmoreParameters::$CARD_LOCH_SHIEL:
+                $this->applyLochShiel($personalBoard);
+                break;
+            case GlenmoreParameters::$CARD_DONAN_CASTLE:
+                $this->applyDonanCastle($playerTileGLM);
+                break;
+            case GlenmoreParameters::$CARD_ARMADALE_CASTLE:
+                $this->applyArmadaleCastle($personalBoard);
+                break;
+            case GlenmoreParameters::$CARD_LOCH_LOCHY:
+                $this->applyLochLochy($playerTileGLM);
+                break;
+            case GlenmoreParameters::$CARD_LOCH_OICH:
+                $this->applyLochOich($personalBoard);
+                break;
+            case GlenmoreParameters::$CARD_CASTLE_MOIL:
+                $this->applyCastleMoil($playerTileGLM);
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
      * applyIonaAbbey: applies effect of card Iona Abbey
+     *
      * @param GameGLM $gameGLM
      * @return void
      */
-    public function applyIonaAbbey(GameGLM $gameGLM) : void
+    public function applyIonaAbbey(GameGLM $gameGLM): void
     {
         $this->applyEndGameCard($gameGLM, GlenmoreParameters::$CARD_IONA_ABBEY,
             GlenmoreParameters::$TILE_TYPE_YELLOW, GlenmoreParameters::$IONA_ABBEY_POINTS);
@@ -41,10 +85,11 @@ class CardGLMService
 
     /**
      * applyDuartCastle: applies effect of card Duart Castle
+     *
      * @param GameGLM $gameGLM
      * @return void
      */
-    public function applyDuartCastle(GameGLM $gameGLM) : void
+    public function applyDuartCastle(GameGLM $gameGLM): void
     {
         $this->applyEndGameCard($gameGLM, GlenmoreParameters::$CARD_DUART_CASTLE,
             GlenmoreParameters::$TILE_TYPE_VILLAGE, GlenmoreParameters::$DUART_CASTLE_POINTS);
@@ -52,10 +97,11 @@ class CardGLMService
 
     /**
      * applyLochMorar: applies effect of card Loch Morar
+     *
      * @param GameGLM $gameGLM
      * @return void
      */
-    public function applyLochMorar(GameGLM $gameGLM) : void
+    public function applyLochMorar(GameGLM $gameGLM): void
     {
         $this->applyEndGameCard($gameGLM, GlenmoreParameters::$CARD_LOCH_MORAR,
             GlenmoreParameters::$TILE_TYPE_GREEN, GlenmoreParameters::$LOCH_MORAR_POINTS);
@@ -64,14 +110,15 @@ class CardGLMService
 
     /**
      * applyEndGameCard : applies effect of special cards for points count at the end of the game.
-     * @param GameGLM $gameGLM the game
-     * @param String  $cardName the name of the special card
-     * @param String  $tileColor the color of the tiles impacted by the card
+     *
+     * @param GameGLM $gameGLM    the game
+     * @param String  $cardName   the name of the special card
+     * @param String  $tileColor  the color of the tiles impacted by the card
      * @param int     $cardPoints the amount of points given by the card
      * @return void
      */
-    private function applyEndGameCard(GameGLM $gameGLM, String $cardName,
-        String $tileColor, int $cardPoints) : void
+    private function applyEndGameCard(GameGLM $gameGLM, string $cardName,
+        string $tileColor, int $cardPoints): void
     {
         $players = $gameGLM->getPlayers();
         $owns = false;
@@ -94,4 +141,36 @@ class CardGLMService
         }
         $this->entityManager->flush();
     }
+
+    /**
+     * applyCastleMoil : gives a whisky barrel to the player
+     * @param PlayerTileGLM $playerTileGLM
+     * @return void
+     */
+    private function applyCastleMoil(PlayerTileGLM $playerTileGLM) : void
+    {
+        $resource = $this->resourceGLMRepository->findOneBy(["type" => GlenmoreParameters::$WHISKY_RESOURCE]);
+        $playerTileResource = new PlayerTileResourceGLM();
+        $playerTileResource->setResource($resource);
+        $playerTileResource->setQuantity(1);
+        $playerTileResource->setPlayerTileGLM($playerTileGLM);
+        $this->entityManager->persist($playerTileResource);
+        $playerTileGLM->addPlayerTileResource($playerTileResource);
+        $this->entityManager->persist($playerTileGLM);
+        $this->entityManager->persist($playerTileGLM->getPersonalBoard());
+        $this->entityManager->flush();
+    }
+
+    /**
+     * applyArmadaleCastle : gives 3 money coins to the player
+     * @param PersonalBoardGLM $personalBoardGLM
+     * @return void
+     */
+    private function applyArmadaleCastle(PersonalBoardGLM $personalBoardGLM) : void
+    {
+        $personalBoardGLM->setMoney($personalBoardGLM->getMoney() + 3);
+        $this->entityManager->persist($personalBoardGLM);
+        $this->entityManager->flush();
+    }
+
 }
