d9886e84459b3e7cd02fc4cf55f04556abbe0f04
vanliflo <florian.van-liedekerke@univ-rouen.fr>
2024-03-09 10:05:06 +0100

    Feature: added a new entity and Loch Lochy Management for service

---
+++ app/src/Entity/Game/Glenmore/SelectedResourceGLM.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Entity\Game\Glenmore;
+
+use App\Repository\Game\Glenmore\SelectedResourceGLMRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SelectedResourceGLMRepository::class)]
+class SelectedResourceGLM
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?ResourceGLM $resource = null;
+
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\ManyToOne(inversedBy: 'selectedResources')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?PersonalBoardGLM $personalBoardGLM = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getResource(): ?ResourceGLM
+    {
+        return $this->resource;
+    }
+
+    public function setResource(?ResourceGLM $resource): static
+    {
+        $this->resource = $resource;
+
+        return $this;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(int $quantity): static
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPersonalBoardGLM(): ?PersonalBoardGLM
+    {
+        return $this->personalBoardGLM;
+    }
+
+    public function setPersonalBoardGLM(?PersonalBoardGLM $personalBoardGLM): static
+    {
+        $this->personalBoardGLM = $personalBoardGLM;
+
+        return $this;
+    }
+}
