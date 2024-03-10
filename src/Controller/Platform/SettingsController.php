a33b96d4efae7b628c50d4395f5ec09efc5b4005
Mohamed Cherfi <mohamed.cherfi@univ-rouen.fr>
2024-03-10 15:46:13 +0100

    première version de la page paramètres de compte

--- src/Controller/Platform/SettingsController.php
+++ src/Controller/Platform/SettingsController.php
@@ -2,22 +2,25 @@
 
 namespace App\Controller\Platform;
 
+use App\Entity\Platform\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SettingsController extends AbstractController
 {
-    /**
-     * Display the settings page
-     * 
-     * @return Response HTTP response: settings page
-     */
-    #[Route('/settings', name: 'app_settings')]
+    #[Route('/dashboard/user/settings', name: 'app_settings')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
+        $username = $user->getUsername();
+        $email = $user->getEmail();
+
         return $this->render('platform/users/settings.html.twig', [
             'controller_name' => 'SettingsController',
+            'username' => $username,
+            'email' => $email,
         ]);
     }
 }
