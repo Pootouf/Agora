02bfeff808121df0460aa33ff864a9bf0137a4b7
khaboiza <iza.khabouri@gmail.com>
2024-03-26 14:33:24 +0100

    feature: fix manage birth bonus and harvest

--- app/src/Service/Game/Myrmes/MYRService.php
+++ app/src/Service/Game/Myrmes/MYRService.php
@@ -10,21 +10,25 @@ use App\Entity\Game\Myrmes\NurseMYR;
 use App\Entity\Game\Myrmes\PlayerMYR;
 use App\Entity\Game\Myrmes\PreyMYR;
 use App\Entity\Game\Myrmes\SeasonMYR;
+use App\Repository\Game\Myrmes\NurseMYRRepository;
 use App\Entity\Game\Myrmes\TileMYR;
 use App\Entity\Game\Myrmes\TileTypeMYR;
 use App\Repository\Game\Myrmes\PlayerMYRRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use App\Repository\Game\Myrmes\TileMYRRepository;
 use App\Repository\Game\Myrmes\TileTypeMYRRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 
 class MYRService
 {
 
-    public function __construct(private readonly PlayerMYRRepository $playerMYRRepository,
+    public function __construct(private PlayerMYRRepository $playerMYRRepository,
+                private EntityManagerInterface $entityManager,
+                private readonly NurseMYRRepository $nurseMYRRepository,
                 private readonly TileMYRRepository $tileMYRRepository,
-                private readonly TileTypeMYRRepository $tileTypeMYRRepository,
-                private readonly EntityManagerInterface $entityManager)
+                private readonly TileTypeMYRRepository $tileTypeMYRRepository )
     {
 
     }
@@ -265,4 +269,52 @@ class MYRService
         $this->entityManager->flush();
     }
 
+    /**
+     * getNursesAtPosition : return nurses which is in $position
+     * @param PlayerMYR $player
+     * @param int $position
+     * @return ArrayCollection
+     */
+    public function getNursesAtPosition(PlayerMYR $player, int $position): ArrayCollection
+    {
+        $nurses =  $this->nurseMYRRepository->findBy(["position" => $position,
+            "player" => $player]);
+        return new ArrayCollection($nurses);
+    }
+
+    /**
+     * manageNursesAfterBonusGive : Replace use nurses
+     * @param PlayerMYR $player
+     * @param int $nurseCount
+     * @param int $positionOfNurse
+     * @return void
+     * @throws Exception
+     */
+    public function manageNursesAfterBonusGive(PlayerMYR $player, int $nurseCount, int $positionOfNurse) : void
+    {
+        if ($nurseCount > 0) {
+            $nurses = $this->getNursesAtPosition($player, $positionOfNurse);
+            foreach ($nurses as $n) {
+                if ($nurseCount == 0) {
+                    return;
+                }
+                switch ($positionOfNurse) {
+                    case MyrmesParameters::$LARVAE_AREA:
+                    case MyrmesParameters::$SOLDIERS_AREA:
+                    case MyrmesParameters::$WORKER_AREA:
+                    case MyrmesParameters::$WORKSHOP_ANTHILL_HOLE_AREA:
+                    case MyrmesParameters::$WORKSHOP_LEVEL_AREA:
+                    case MyrmesParameters::$WORKSHOP_NURSE_AREA:
+                        $n->setPosition(MyrmesParameters::$BASE_AREA);
+                        $this->entityManager->persist($n);
+                        break;
+                    case MyrmesParameters::$WORKSHOP_GOAL_AREA:
+                        break;
+                    default:
+                        throw new Exception("Don't manage bonus");
+                }
+                $nurseCount--;
+            }
+        }
+    }
 }
