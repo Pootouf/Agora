4c3ffe04d74354effbec52e98e061743f3b49ea4
ellexart <axelle.mazuy@gmail.com>
2024-03-27 18:54:05 +0100

    AV-383 feature: add button to show main board

---
+++ app/assets/controllers/myrmes_controller.js
@@ -0,0 +1,23 @@
+import { Controller } from '@hotwired/stimulus';
+
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+    async showMainBoard(main)  {
+        let url = main.params.url;
+        const response = await fetch(url);
+        let tree = document.getElementById("index_myrmes");
+        let placeholder = document.createElement("div");
+        placeholder.innerHTML = await response.text();
+        const node = placeholder.firstElementChild;
+        tree.appendChild(node);
+    }
+
+}
