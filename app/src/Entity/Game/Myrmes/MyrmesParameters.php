35e432bc42217207895c43e71c5cf086a6e6a300
khaboiza <iza.khabouri@gmail.com>
2024-03-28 11:35:27 +0100

    feature: add parameters for all buy harvest

--- app/src/Entity/Game/Myrmes/MyrmesParameters.php
+++ app/src/Entity/Game/Myrmes/MyrmesParameters.php
@@ -20,9 +20,11 @@ class MyrmesParameters
     public static int $PHASE_WORKSHOP = 4;
 
     // Player color
+
     public static array $PLAYERS_COLORS = ['blue', 'yellow', 'red', 'gray'];
 
     // Area's for nurses
+
     public static int $BASE_AREA = 0;
     public static int $LARVAE_AREA = 1;
     public static int $SOLDIERS_AREA = 2;
@@ -33,18 +35,28 @@ class MyrmesParameters
     public static int $WORKSHOP_NURSE_AREA = 7;
     public static int $AREA_COUNT = 8;
 
-
-
     //Nurses parameters
+
     public static int $START_NURSES_COUNT_PER_PLAYER = 3;
     public static int $MAX_NURSES_COUNT_PER_PLAYER = 8;
 
-
-
     // Win by area's nurses
-    public static array $WIN_LARVAE_BY_NURSES_COUNT = [0, 1, 3, 5];
-    public static array $WIN_SOLDIERS_BY_NURSES_COUNT = [0, 0, 1, 1];
-    public static array $WIN_WORKERS_BY_NURSES_COUNT = [0, 0, 1, 0, 2];
+
+    public static array $WIN_LARVAE_BY_NURSES_COUNT = array(
+        1 => 1,
+        2 => 3,
+        3 => 5
+    );
+
+    public static array $WIN_SOLDIERS_BY_NURSES_COUNT = array(
+        2 => 1,
+        3 => 2
+    );
+
+    public static array $WIN_WORKERS_BY_NURSES_COUNT = array(
+        2 => 1,
+        4 => 2
+    );
 
     // Events bonus
 
@@ -57,34 +69,46 @@ class MyrmesParameters
     public static int $BONUS_PHEROMONE = 6;
     public static int $BONUS_WORKER = 7;
 
-
     // Bonus harvested pheromone
 
     public static int $HARVESTED_TILE_BONUS = 3;
 
-
-
     // Years
+
     public static int $FIRST_YEAR_NUM = 1;
     public static int $SECOND_YEAR_NUM = 2;
     public static int $THIRD_YEAR_NUM = 3;
 
-
-
     // Seasons
+
     public static string $SPRING_SEASON_NAME = "spring";
     public static string $SUMMER_SEASON_NAME = "summer";
     public static string $WINTER_SEASON_NAME = "winter";
     public static string $FALL_SEASON_NAME = "fall";
     public static string $INVALID_SEASON_NAME = "invalid";
 
+    // TILE TYPES
 
-    //TILE TYPES
     public static string $WATER_TILE_TYPE = "water";
     public static string $DIRT_TILE_TYPE = "dirt";
     public static string $MUSHROOM_TILE_TYPE = "mushroom";
     public static string $STONE_TILE_TYPE = "stone";
     public static string $GRASS_TILE_TYPE = "grass";
+
+    // Buy for add anthill level
+
+    public static array $BUY_RESOURCE_FOR_LEVEL_ONE = array(
+        "grass" => 2
+    );
+
+    public static array $BUY_RESOURCE_FOR_LEVEL_TWO = array(
+        "grass" => 2,
+        "stone" => 1
+    );
+
+    public static array $BUY_RESOURCE_FOR_LEVEL_THREE = array(
+        "stone" => 3
+    );
 
     // Pheromone Level
 
@@ -144,24 +168,19 @@ class MyrmesParameters
 
 
     //Prey Types
+
     public static string $LADYBUG_TYPE = "ladybug";
     public static string $TERMITE_TYPE = "termite";
     public static string $SPIDER_TYPE = "spider";
-
-
 
     //Prey Numbers
     public static int $LADYBUG_NUMBER = 6;
     public static int $TERMITE_NUMBER = 6;
     public static int $SPIDER_NUMBER = 6;
 
-
-
     //Prey Position
     public static array $PREY_POSITIONS = [[1, 6], [1, 18], [4, 9], [4, 15], [5, 12], [6, 9], [6, 15], [7, 0], [7, 6],
         [7, 18], [7, 24], [8, 9], [8, 15], [9, 12], [10, 9], [10, 15], [13, 6], [13, 18]];
-
-
 
     //Player start data
     public static int $NUMBER_OF_WORKER_AT_START = 2;
@@ -169,16 +188,12 @@ class MyrmesParameters
     public static int $ANTHILL_START_LEVEL = 0;
     public static int $PLAYER_START_SCORE = 10;
 
-
-
     //Anthill hole start position
     public static array $ANTHILL_HOLE_POSITION_BY_NUMBER_OF_PLAYER = [
         2 => [[11, 6], [11, 18]],
         3 => [[5, 4], [4, 19], [12, 13]],
         4 => [[3, 6], [3, 18], [11, 6], [11, 18]]
     ];
-
-
 
     //Excluded tile with 2 players
     public static array $EXCLUDED_TILES_2_PLAYERS = [
@@ -192,6 +207,5 @@ class MyrmesParameters
     public static string $RESOURCE_TYPE_STONE = "stone";
     public static string $RESOURCE_TYPE_GRASS = "grass";
     public static string $RESOURCE_TYPE_LARVAE = "larvae";
-
 
 }
