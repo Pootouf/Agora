70bf74e30b8e78482f81e5462f37ca6e36e08532
lehucher <robin.lehucher@univ-rouen.fr>
2024-04-01 17:15:07 +0200

    creating function __toString, for the display in the board creation form

--- app/src/Entity/Platform/User.php
+++ app/src/Entity/Platform/User.php
@@ -380,4 +380,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->username;
+    }
 }
