200f72be3fff49026120b601aa052cbb81754be0
Mohamed Cherfi <mohamed.cherfi@univ-rouen.fr>
2024-04-08 14:08:06 +0200

    mise a jour des 2 pages administration

--- app/src/Controller/Platform/DashboardController.php
+++ app/src/Controller/Platform/DashboardController.php
@@ -230,10 +230,6 @@ class DashboardController extends AbstractController
     }
 
     /**
-     * Displays a list of all users.
-     *
-     * This method retrieves all games from the database using the entity manager (when the user is connected)
-     *
      * @param EntityManagerInterface $entityManager The entity manager to interact with the database
      *
      * @return Response  HTTP response: list of games page
@@ -247,6 +243,25 @@ class DashboardController extends AbstractController
 
         // Passer la liste des utilisateurs à votre modèle de vue
         return $this->render('platform/dashboard/allusers.html.twig', [
+            'users' => $users,
+            'notifications' => $this->notifications, // Assurez-vous que vos notifications sont également disponibles dans ce contrôleur
+        ]);
+    }
+
+     /**
+     * @param EntityManagerInterface $entityManager The entity manager to interact with the database
+     *
+     * @return Response  HTTP response: list of games page
+     */
+    #[Route('/dashboard/banmanager', name: 'app_dashboard_banmanager')]
+    public function dashboard_banmanager(EntityManagerInterface $entityManager): Response
+    {
+        // Récupérer tous les utilisateurs à partir de votre source de données (par exemple, une entité User)
+        $userRepository = $entityManager->getRepository(User::class);
+        $users = $userRepository->findAll();
+
+        // Passer la liste des utilisateurs à votre modèle de vue
+        return $this->render('platform/dashboard/banmanager.html.twig', [
             'users' => $users,
             'notifications' => $this->notifications, // Assurez-vous que vos notifications sont également disponibles dans ce contrôleur
         ]);
