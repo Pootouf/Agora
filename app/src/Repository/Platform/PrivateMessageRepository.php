7ecb12e94fc0d4b239e13f9f47bd97d56e466458
MisterGit <anass.elgharbaoui@usmba.ac.ma>
2024-04-11 15:11:11 +0200

    user and Message Repository

---
+++ app/src/Repository/Platform/PrivateMessageRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\Repository\Platform;
+
+use App\Entity\Platform\PrivateMessage;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<PrivateMessage>
+ *
+ * @method PrivateMessage|null find($id, $lockMode = null, $lockVersion = null)
+ * @method PrivateMessage|null findOneBy(array $criteria, array $orderBy = null)
+ * @method PrivateMessage[]    findAll()
+ * @method PrivateMessage[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class PrivateMessageRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, PrivateMessage::class);
+    }
+
+//    /**
+//     * @return PrivateMessage[] Returns an array of PrivateMessage objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('p.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?PrivateMessage
+//    {
+//        return $this->createQueryBuilder('p')
+//            ->andWhere('p.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
