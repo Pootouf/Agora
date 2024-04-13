45812e61e7f054e755c5e055e452d2d91525479a
Michel Nassalang <nassalang.michel@gmail.com>
2024-04-13 13:01:17 +0200

    Add redirection according to roles

--- app/src/Controller/Platform/SecurityController.php
+++ app/src/Controller/Platform/SecurityController.php
@@ -20,7 +20,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if($this->getUser()) {
-            return $this->redirectToRoute('app_dashboard_user');
+            return $this->redirectToRoute('app_home');
         }
 
         // get the login error if there is one
