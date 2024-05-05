4d4979ca08fab5b8da143a6297a65b655733f712
Michel Nassalang <nassalang.michel@gmail.com>
2024-05-05 13:41:16 +0200

    Add migration for admin account

--- app/src/DataFixtures/UserPlatformFixtures.php
+++ app/src/DataFixtures/UserPlatformFixtures.php
@@ -31,27 +31,27 @@ class UserPlatformFixtures extends Fixture
             $users[] = $user;
         }
 
-        // Moderator
-        $moderator = new User();
-        $moderator->setUsername('moderator');
-        $moderator->setEmail("moderator@univ-rouen.fr");
-        $moderator->setIsVerified(true);
-        $password = $this->hasher->hashPassword($moderator, 'moderagora');
-        $moderator->setPassword($password);
-        $moderator->setRoles(['ROLE_MODERATOR']);
-        $manager->persist($moderator);
-        $users[] = $moderator;
-
-        // Administrator
-        $admin = new User();
-        $admin->setUsername('admin');
-        $admin->setEmail("admin@univ-rouen.fr");
-        $admin->setIsVerified(true);
-        $password = $this->hasher->hashPassword($admin, 'adminagora');
-        $admin->setPassword($password);
-        $admin->setRoles(['ROLE_ADMIN']);
-        $manager->persist($admin);
-        $users[] = $admin;
+//        // Moderator
+//        $moderator = new User();
+//        $moderator->setUsername('moderator');
+//        $moderator->setEmail("moderator@univ-rouen.fr");
+//        $moderator->setIsVerified(true);
+//        $password = $this->hasher->hashPassword($moderator, 'moderagora');
+//        $moderator->setPassword($password);
+//        $moderator->setRoles(['ROLE_MODERATOR']);
+//        $manager->persist($moderator);
+//        $users[] = $moderator;
+//
+//        // Administrator
+//        $admin = new User();
+//        $admin->setUsername('admin');
+//        $admin->setEmail("admin@univ-rouen.fr");
+//        $admin->setIsVerified(true);
+//        $password = $this->hasher->hashPassword($admin, 'adminagora');
+//        $admin->setPassword($password);
+//        $admin->setRoles(['ROLE_ADMIN']);
+//        $manager->persist($admin);
+//        $users[] = $admin;
 
 
         $manager->flush();
