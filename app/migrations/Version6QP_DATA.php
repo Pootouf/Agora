f7177bc4d5d09c3a6ecd7c2de066e3a6fb8a034a
Etienne Binginot <etienne.binginot@univ-rouen.fr>
2024-05-06 21:30:19 +0200

    fix: fix name of 6QP migration

--- app/migrations/Version6QP_DATA.php
+++ app/migrations/Version6QP_DATA.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version6QPDATA extends AbstractMigration
+final class Version6QP_DATA extends AbstractMigration
 {
     public function getDescription(): string
     {
