e8051372d96b8c5576020012a3aaf04d8143183b
lehucrob <61794727+lehucrob@users.noreply.github.com>
2024-06-05 12:13:21 +0200

    Deleted files

--- assets/controllers/gameNavigationMenu_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from '@hotwired/stimulus';
-
-export default class extends Controller {
-    async excludePlayer(data) {
-        let url = data.params.url;
-        await fetch(url)
-        document.location.href = '/'
-    }
-}
--- assets/scripts/Myrmes/InvalidSoldierNumberException.js
+++
@@ -1,1 +0,0 @@
-class InvalidSoldierNumberException extends Error{}
